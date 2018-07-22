882ad9855f3e002add135459b86ec3fd7d6c5c65
Ryan Churchill <ryan.churchill@gmail.com>
2018-07-22 15:00:45 -0700

    164-1

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -2,6 +2,7 @@
 
 #include "TankMovementComponent.h"
 
-
-
-
+void UTankMovementComponent::IntendMoveForward(float Throw)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Intend move forward; Throw: %f"), Throw);
+}
