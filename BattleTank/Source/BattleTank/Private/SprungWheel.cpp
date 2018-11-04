9117de697aaa007b08152306e59b12163ecbb20f
Ryan Churchill <ryan.churchill@gmail.com>
2018-11-04 12:30:47 -0800

    216 Converting the spring to C++

---
+++ BattleTank/Source/BattleTank/Private/SprungWheel.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SprungWheel.h"
+#include "Components/StaticMeshComponent.h"
+#include "PhysicsEngine/PhysicsConstraintComponent.h"
+
+// Sets default values
+ASprungWheel::ASprungWheel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	Mass = CreateDefaultSubobject<UStaticMeshComponent>(FName("Mass"));
+	SetRootComponent(Mass);
+
+	Wheel = CreateDefaultSubobject<UStaticMeshComponent>(FName("Wheel"));
+	Wheel->SetupAttachment(Mass);
+
+	MassWheelConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("MassWheelConstraint"));
+	MassWheelConstraint->SetupAttachment(Mass);
+	MassWheelConstraint->SetConstrainedComponents(Mass, FName(""), Wheel, FName(""));
+
+}
+
+// Called when the game starts or when spawned
+void ASprungWheel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASprungWheel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
